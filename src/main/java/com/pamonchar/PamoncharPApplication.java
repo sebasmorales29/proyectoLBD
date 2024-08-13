6533a43a3d2cb8302877dfdf1d1f52fcb74965d7
JeanPaulo21 <126036883+JeanPaulo21@users.noreply.github.com>
2024-08-13 17:13:10 -0600

    Nuevo Projecto

---
+++ src/main/java/com/pamonchar/PamoncharPApplication.java
@@ -0,0 +1,13 @@
+package com.pamonchar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamoncharPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamoncharPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pamonchar/PamoncharPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamonchar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamoncharPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
