9df05efb72e58ec0be609083036a8738bd62c9be
JeanPaulo21 <126036883+JeanPaulo21@users.noreply.github.com>
2024-12-17 21:26:40 -0600

    vistas
    Vistas actualizadas

--- sql/Vistas/Vistas.sql
+++ sql/Vistas/Vistas.sql
@@ -1,77 +1,33 @@
 --VISTAS
 
---categoria
-CREATE OR REPLACE VIEW v_categoria AS
-SELECT id_categoria, descripcion,
-    CASE 
-        WHEN activo = 'Y' THEN 'Activa'
-        ELSE 'Inactiva'
-    END AS estado
+CREATE OR REPLACE VIEW vw_categoria AS
+SELECT id_categoria, descripcion, activo
 FROM Categoria;
-/
 
+CREATE OR REPLACE VIEW vw_producto AS
+SELECT id_producto, nombre, descripcion, precio, existencias, activo, id_categoria
+FROM Producto;
 
+CREATE OR REPLACE VIEW vw_ofertas AS
+SELECT id_ofertas, nombre, descripcion, activo
+FROM Ofertas;
 
+CREATE OR REPLACE VIEW vw_rol AS
+SELECT id_rol, nombre
+FROM Rol;
 
+CREATE OR REPLACE VIEW vw_usuario AS
+SELECT id_usuario, nombre_usuario, nombre, prim_apellido, seg_apellido, correo, id_rol, activo
+FROM Usuario;
 
---producto
-CREATE OR REPLACE VIEW v_producto AS
-SELECT p.id_producto,p.nombre,p.descripcion,p.precio,p.existencias,c.descripcion AS categoria,
-    CASE 
-        WHEN p.activo = 'Y' THEN 'Activa'
-        ELSE 'Inactiva'
-    END AS estado
-FROM Producto p
-LEFT JOIN Categoria c ON p.id_categoria = c.id_categoria;
-/
---direcciones
-CREATE OR REPLACE VIEW v_direccion AS
-SELECT d.id_direccion,d.provincia,d.ciudad,d.ubicacion_exacta,u.nombre_usuario AS usuario
-FROM Direccion d
-LEFT JOIN Usuario u ON d.id_usuario = u.id_usuario;
-/
---Telfono
-CREATE OR REPLACE VIEW v_telefonos AS
-SELECT 
-    t.id_telefono,
-    t.numero_telefono,
-    r.nombre AS rol
-FROM 
-    Telefono t
-LEFT JOIN 
-    Usuario u ON t.id_usuario = u.id_usuario
-LEFT JOIN 
-    Rol r ON u.id_rol = r.id_rol;
-/
---Usuarios
-CREATE OR REPLACE VIEW v_usuario AS
-SELECT 
-    u.id_usuario,
-    u.nombre_usuario,
-    u.nombre,
-    u.prim_apellido,
-    u.seg_apellido,
-    u.correo,
-    u.id_rol,
-    r.nombre AS rol,
-    CASE 
-        WHEN u.activo = 'Y' THEN 'Activa'
-    ELSE 'Inactiva'
-END AS estado
-FROM 
-    Usuario u
-LEFT JOIN Rol r ON u.id_rol = r.id_rol;
-/
---Ofertas
-CREATE OR REPLACE VIEW v_ofertas AS
-SELECT 
-    id_ofertas,
-    nombre,
-    descripcion,
-    imagen,
-    CASE 
-        WHEN activo = 'Y' THEN 'Activa'
-        ELSE 'Inactiva'
-    END AS estado
-FROM 
-    Ofertas;
+CREATE OR REPLACE VIEW vw_factura AS
+SELECT id_factura, fecha, total, id_usuario
+FROM Factura;
+
+CREATE OR REPLACE VIEW vw_detalle_factura AS
+SELECT id_venta, precio, cantidad, id_factura, id_producto
+FROM Detalle_Factura;
+
+CREATE OR REPLACE VIEW vw_historial_ventas AS
+SELECT id_historial_ventas, fecha_venta, id_usuario
+FROM Historial_Ventas;
